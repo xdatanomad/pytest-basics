472846582f2fe6e1103add24c1c5103ca04da476
parham <parham@datastack.academy>
2025-01-15 20:23:28 -0500

    adding tests

---
+++ tests/test_core.py
@@ -0,0 +1,16 @@
+import pytest
+from gitallica.core import Gitallica
+
+@pytest.mark.basic
+def test_add():
+    g = Gitallica()
+    assert g.add(1, 2) == 3
+    assert g.add(-1, 1) == 0
+    assert g.add(-1, -1) == -2
+
+@pytest.mark.advanced
+def test_subtract():
+    g = Gitallica()
+    assert g.subtract(1, 2) == -1
+    assert g.subtract(-1, 1) == -2
+    assert g.subtract(-1, -1) == 0
